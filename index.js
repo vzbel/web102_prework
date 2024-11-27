e9191b7ad29f88d10feeacb2f235c608f1925a3d
vzbel <142253332+vzbel@users.noreply.github.com>
2024-11-27 13:01:00 -0700

    Finish challenge 4

--- index.js
+++ index.js
@@ -63,16 +63,26 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+let contributions = GAMES_JSON.reduce((acc, game) => {
+  return acc + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `${contributions.toLocaleString("en-US")}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+let moneyPledged = GAMES_JSON.reduce((acc, game) => {
+  return acc + game.pledged;
+}, 0);
 
 // set inner HTML using template literal
+raisedCard.innerHTML = `$${moneyPledged.toLocaleString("en-US")}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+let numberOfGames = GAMES_JSON.length;
+gamesCard.innerHTML = `${numberOfGames.toLocaleString("en-US")}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
