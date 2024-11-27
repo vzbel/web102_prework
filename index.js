c0de4d49d63ec2886939aa97d68ed87a6230dcd7
vzbel <142253332+vzbel@users.noreply.github.com>
2024-11-27 13:10:46 -0700

    Finish challenge 5

--- index.js
+++ index.js
@@ -95,8 +95,12 @@ function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have not yet met their goal
+  let unfundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal;
+  });
 
   // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
@@ -104,8 +108,12 @@ function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
   // use filter() to get a list of games that have met or exceeded their goal
+  let fundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged >= game.goal;
+  });
 
   // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(fundedGames);
 }
 
 // show all games
@@ -113,6 +121,7 @@ function showAllGames() {
   deleteChildElements(gamesContainer);
 
   // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -121,6 +130,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
